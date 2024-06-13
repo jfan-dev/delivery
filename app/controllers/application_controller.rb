1e539c2b5a63a36b3ed3e0a8c2f2d7554fb1fb89
jfan-dev <jfan.dev+github@gmail.com>
2024-06-13 20:33:58 -0300

    :closed_lock_with_key: Implement token authentication in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  def authenticate!
+    if request.format.json?
+      check_token!
+    else
+      authenticate_user!
+    end
+  end
+
+  private
+
+  def check_token!
+    if user = authenticate_with_http_token { |token, _| User.from_token(token) }
+      @current_user = user
+    else
+      render json: { message: "Not authorized" }, status: 401
+    end
+  end
 end
