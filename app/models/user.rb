86ce1c3e5244b7b558078ce3581d1a88c99fbc4a
jfan-dev <jfan.dev+github@gmail.com>
2024-06-13 19:24:34 -0300

    :bricks: Update User model with role-based methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  enum role: { admin: 0, seller: 1, buyer: 2 }
   has_many :stores
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 end
