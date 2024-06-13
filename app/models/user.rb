3cab9ac96695b2a6c9155588f947e37178a5f99f
jfan-dev <jfan.dev+github@gmail.com>
2024-06-13 20:32:48 -0300

    🔐 :closed_lock_with_key: Add JWT token generation and decoding methods to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,18 @@
 class User < ApplicationRecord
+  
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   enum role: { admin: 0, seller: 1, buyer: 2 }
   has_many :stores
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  def self.token_for(user)
+    payload = { id: user.id, email: user.email, role: user.role }
+    JWT.encode payload, "muito.secreto", "HS256"
+  end
+
+  def self.from_token(token)
+    payload = JWT.decode(token, "muito.secreto", true, { algorithm: 'HS256' }).first
+    find_by(id: payload["id"])
+  end
 end
