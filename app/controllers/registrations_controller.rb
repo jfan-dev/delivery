a8987bd30a88c6ea29fa8fea176c5ce415942f48
jfan-dev <jfan.dev+github@gmail.com>
2024-06-13 20:50:41 -0300

    :sparkles: Implement user registration and authentication in RegistrationsController

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,6 @@
 class RegistrationsController < ApplicationController
-  skip_forgery_protection only: [:create]
+  skip_forgery_protection only: [:create, :sign_in, :me]
+  before_action :authenticate!, only: [:me]
 
   def create
     @user = User.new(user_params)
@@ -10,9 +11,27 @@ class RegistrationsController < ApplicationController
     end
   end
 
+  def sign_in
+    user = User.find_by(email: sign_in_params[:email])
+    if user&.valid_password?(sign_in_params[:password])
+      token = User.token_for(user)
+      render json: { email: user.email, token: token }
+    else
+      render json: { message: "Nope!" }, status: 401
+    end
+  end
+
+  def me
+    render json: { email: current_user.email, role: current_user.role }
+  end
+
   private
 
   def user_params
-    params.required(:user).permit(:email, :password, :password_confirmation, :role)
+    params.require(:user).permit(:email, :password, :password_confirmation, :role)
+  end
+
+  def sign_in_params
+    params.require(:login).permit(:email, :password)
   end
 end
