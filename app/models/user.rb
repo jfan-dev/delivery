0e86843a607778879a20a88b4153a8663401c296
jfan-dev <jfan.dev+github@gmail.com>
2024-06-13 21:30:44 -0300

    :closed_lock_with_key: Add JWT token generation with expiration to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,9 @@ class User < ApplicationRecord
   has_many :stores
 
   def self.token_for(user)
+    jwt_headers = { exp: 1.hour.from_now.to_i }
     payload = { id: user.id, email: user.email, role: user.role }
-    JWT.encode payload, "muito.secreto", "HS256"
+    JWT.encode payload.merge(jwt_headers), "muito.secreto", "HS256"
   end
 
   def self.from_token(token)
