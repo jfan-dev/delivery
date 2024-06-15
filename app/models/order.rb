f5996c66fbdfa0deb139c6d088af5b6c7870619e
jfan-dev <jfan.dev+github@gmail.com>
2024-06-15 08:29:26 -0300

    :wrench: Update Order model with relationships and validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,14 @@
 class Order < ApplicationRecord
   belongs_to :buyer, class_name: "User"
   belongs_to :store
+  has_many :order_items
+  has_many :products, through: :order_items
+
+  validate :buyer_role
+
+  private
+
+  def buyer_role
+    errors.add(:buyer, "should be a `buyer`") unless buyer.buyer?
+  end
 end
