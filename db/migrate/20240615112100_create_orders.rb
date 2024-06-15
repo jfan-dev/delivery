bc1cbc3db229e099f53b4c73b3eac8d56b6cb302
jfan-dev <jfan.dev+github@gmail.com>
2024-06-15 08:28:37 -0300

    :wrench: Generate Order model with references to buyer and store

---
+++ db/migrate/20240615112100_create_orders.rb
@@ -0,0 +1,10 @@
+class CreateOrders < ActiveRecord::Migration[7.1]
+  def change
+    create_table :orders do |t|
+      t.references :buyer, null: false, foreign_key: { to_table: :users }
+      t.references :store, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
