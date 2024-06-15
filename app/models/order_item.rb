b1faacd752aeab7a1c18787f7138d709b187204a
jfan-dev <jfan.dev+github@gmail.com>
2024-06-15 08:35:08 -0300

    :wrench: Update OrderItem model with relationships and validations

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,14 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  validate :store_product
+
+  private
+
+  def store_product
+    if product.store != order.store
+      errors.add(:product, "should belong to `Store`: #{order.store.name}")
+    end
+  end
 end
