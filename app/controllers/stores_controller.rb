a9ca2f6b46458d954222d4e4945ef312cf01cb7a
jfan-dev <jfan.dev+github@gmail.com>
2024-06-15 14:56:45 -0300

    🔧 Update Stores form and controller to allow admins to assign sellers

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -2,20 +2,40 @@ class StoresController < ApplicationController
   skip_forgery_protection only: %i[create update]
   before_action :authenticate!
   before_action :set_store, only: %i[ show edit update destroy ]
+  rescue_from User::InvalidToken, with: :not_authorized
 
-  # GET /stores or /stores.json
   def index
+    @stores = if current_user.seller?
+                Store.where(user: current_user)
+              else
+                Store.joins(:user).where(users: { role: 'seller' })
+              end
+  end
+
+  # GET /stores/1 or /stores/1.json
+  def show
+    @store = Store.find(params[:id])
+    @products = @store.products.page(params[:page]).per(8)
+  end
+
+  # GET /stores/new
+  def new
+    @store = Store.new
     if current_user.admin?
-      @stores = Store.all
-    else
-      @stores = Store.where(user: current_user)
+      @sellers = User.where(role: :seller)
     end
+  end
+
+  # GET /stores/1/edit
+  def edit
   end
 
   # POST /stores or /stores.json
   def create
     @store = Store.new(store_params)
-    @store.user = current_user unless current_user.admin?
+    if !current_user.admin?
+      @store.user = current_user
+    end
 
     respond_to do |format|
       if @store.save
@@ -25,6 +45,29 @@ class StoresController < ApplicationController
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @store.errors, status: :unprocessable_entity }
       end
+    end
+  end
+
+  # PATCH/PUT /stores/1 or /stores/1.json
+  def update
+    respond_to do |format|
+      if @store.update(store_params)
+        format.html { redirect_to store_url(@store), notice: "Store was successfully updated." }
+        format.json { render :show, status: :ok, location: @store }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @store.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /stores/1 or /stores/1.json
+  def destroy
+    @store.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to stores_url, notice: "Store was successfully destroyed." }
+      format.json { head :no_content }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,52 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :stores
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  
+  resources :stores do
+    resources :products, only: [:index]
+    get "/orders/new" => "stores#new_order"
+  end
+  
+  get 'welcome/index'
   get "listing" => "products#listing"
+  get "me" => "registrations#me"
+  get "user_listing" => "registrations#user_listing", as: :user_listing
+  get 'orders/new', to: 'orders#new', as: 'new_order'
 
+  
   post "new" => "registrations#create", as: :create_registration
   post "sign_in" => "registrations#sign_in"
-  get "me" => "registrations#me"
-
+  
   scope :buyers do
     resources :orders, only: [:index, :create, :update, :destroy]
   end
 
-  root to: "welcome#index"
+  resources :registrations, only: [:index] do
+    member do
+      patch :change_role
+    end
+  end
+
+  resources :stores do
+    member do
+      patch :disable_user
+    end
+  end
+
+  resource :cart, only: [:show] do
+    post 'add_item/:product_id', to: 'carts#add_item', as: 'add_item'
+    delete 'remove_item/:cart_item_id', to: 'carts#remove_item', as: 'remove_item'
+  end
+
+  resources :orders, only: [:create, :index, :show]
+  
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  
+  # Defines the root path route ("/")
+  root to: "welcome#index"
 end
