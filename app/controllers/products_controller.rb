ede2b68361e52c333dd5d7f523497237a0a23b5b
jfan-dev <jfan.dev+github@gmail.com>
2024-06-17 14:47:47 -0300

    ♻️ Refactor product fetching and authorization logic in index action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,26 +6,21 @@ class ProductsController < ApplicationController
     unless current_user.admin?
       redirect_to root_path, notice: "No permission for you! 🤪"
     end
-
+    
     @products = Product.includes(:store)
   end
 
   def index
     respond_to do |format|
-      @products = Product.where(store_id: params[:store_id]).order(:title).page(params.fetch(:page, 1))
       
       format.html do
+        @products = fetch_products
         render :index
       end
       format.json do
-        if buyer?
-          page = params.fetch(:page, 1)
+        if authorized_user?
           @store = Store.find(params[:store_id])
-          @products = Product
-            .where(store_id: params[:store_id])
-            .order(:title)
-            .page(page)
-          
+          @products = fetch_products
           render :index
         else
           render json: { message: "Not authorized" }, status: 401
@@ -36,7 +31,19 @@ class ProductsController < ApplicationController
 
   private
 
+  def fetch_products
+    Product.where(store_id: params[:store_id]).order(:title).page(params.fetch(:page, 1))
+  end
+
+  def authorized_user?
+    buyer? || seller?
+  end
+
   def buyer?
     (current_user && current_user.buyer?) && current_credential.buyer?
+  end
+
+  def seller?
+    (current_user && current_user.seller?) && current_credential.seller?
   end
 end
