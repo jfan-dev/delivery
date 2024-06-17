1a62ea2ac8c23affaf20759476edfdcc02a04163
jfan-dev <jfan.dev+github@gmail.com>
2024-06-17 18:48:49 -0300

    🔒 Add validations to Product model for title presence and price numericality

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :store
   has_many :orders, through: :order_items
+
+  validates :title, presence: true
+  validates :price, numericality: { greater_than: 0 }
 end
