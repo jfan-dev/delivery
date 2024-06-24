ca59b8d324398ce215d285c26cdff0621670c035
jfan-dev <jfan.dev+github@gmail.com>
2024-06-24 06:56:48 -0300

    🛠️ Set OrderItem price before validation to prevent NOT NULL

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,9 +2,15 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
 
+  before_validation :set_price
+
   validate :store_product
 
   private
+
+  def set_price
+    self.price = product.price if price.nil?
+  end
 
   def store_product
     if product.store != order.store
